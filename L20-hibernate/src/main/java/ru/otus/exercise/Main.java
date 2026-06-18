b113f452d6916a893b252c446d77ef777ff56c40
enji8 <q@w.r>
2026-06-18 15:16:25 +0300

    L20 - мелкие правки

--- L20-hibernate/src/main/java/ru/otus/exercise/Main.java
+++ L20-hibernate/src/main/java/ru/otus/exercise/Main.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.otus.core.HibernateUtils;
 import ru.otus.exercise.model.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
@@ -11,6 +13,33 @@ public class Main {
     public static void main(String[] args) {
         try (var sessionFactory =
                 HibernateUtils.buildSessionFactory(OtusStudent.class, Avatar.class, EMail.class, Course.class)) {
+/*
+            var em = sessionFactory.createEntityManager();
+            var tx = em.getTransaction();
+            tx.begin();
+            System.out.println("\n\n---------------");
+
+            var avatar = new Avatar(0, "xxxx", null);
+            var student = new OtusStudent(0, "test", avatar, new ArrayList<>(), new ArrayList<>());
+            avatar.setStudent(student);
+            var email = new EMail(0, "test@test.com", student);
+            student.getEmails().add(email);
+
+            em.persist(student);
+            //em.persist(email);
+            em.flush();
+            em.clear();
+
+            System.out.println("\n\n========");
+            var found = em.find(OtusStudent.class, 1);
+
+            System.out.println("\n\n==toStr");
+            System.out.println(found.getEmails().getFirst().getStudent());
+
+            tx.commit();
+
+
+*/
             logger.info("Statistics:{}", sessionFactory.getStatistics());
         }
     }
